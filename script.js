1b22548377a2e26d5345a98f1a32a73904bb14e6
Md Abu Farah <confusedengineer.md@gmail.com>
2026-07-23 12:07:46 +0530

    add render feature

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 const allTodos = [];
 
+function deleteTodo(id){
+    
+    for(let i =0;i<allTodos.length;i++){
+        if(allTodos[i].id == id){
+            allTodos.splice(i,1);
+            break;
+        }
+    }
+    renderTodos();
+}
+
+function renderTodos(){
+    const todos = document.getElementById("todos")
+
+    todos.innerHTML = "";
+
+    for(let i =0;i<allTodos.length;i++){
+        const card = document.createElement('div');
+        const text = document.createElement('p')
+        const editButton = document.createElement('button')
+        const deleteButton = document.createElement('button')
+
+        text.innerHTML = allTodos[i].text;
+        editButton.innerHTML = "Edit";
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click",() =>{
+            deleteTodo(allTodos[i].id)
+        })
+        card.appendChild(text)
+        card.appendChild(editButton)
+        card.appendChild(deleteButton);
+        todos.appendChild(card);
+    }
+}
+
 function addTodo(){
     const newTodo = document.getElementById("newTodo");
     const value = newTodo.value.trim();
